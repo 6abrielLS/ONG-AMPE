c0d72a8aef7e21decc599dfefefbdfba7599084d
gustavo-dm <gustavothemoraes@gmail.com>
2025-10-29 18:20:16 -0300

    added app to settings.py, added general urls for project

--- OngAmpaSite/OngAmpaSite/urls.py
+++ OngAmpaSite/OngAmpaSite/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('admin/', admin.site.urls),
   
 ]
